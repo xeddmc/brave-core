c1e56e620be627ebe24ce5984199c7bd41001f08
Snuupy <blee@brave.com>
2019-07-02 17:35:07 -0700

    isIdempotent

--- components/brave_extension/extension/brave_extension/background/api/cosmeticFilterAPI.ts
+++ components/brave_extension/extension/brave_extension/background/api/cosmeticFilterAPI.ts
@@ -4,13 +4,34 @@
 import { Tab } from '../../types/state/shieldsPannelState'
 // import * as shieldsPanelState from '../../state/shieldsPanelState'
 
+// export const addSiteCosmeticFilter = async (origin: string, cssfilter: string) => {
+//   chrome.storage.local.get('cosmeticFilterList', (storeData = {}) => {
+//     let storeList = Object.assign({}, storeData.cosmeticFilterList)
+//     if (storeList[origin] === undefined || storeList[origin].length === 0) { // nothing in filter list for origin
+//       storeList[origin] = [cssfilter]
+//     } else { // add entry
+//       storeList[origin].push(cssfilter)
+//     }
+//     chrome.storage.local.set({ 'cosmeticFilterList': storeList })
+//   })
+// }
+
 export const addSiteCosmeticFilter = async (origin: string, cssfilter: string) => {
+  console.log('test API SITE COSMETIC FILTER')
   chrome.storage.local.get('cosmeticFilterList', (storeData = {}) => {
     let storeList = Object.assign({}, storeData.cosmeticFilterList)
     if (storeList[origin] === undefined || storeList[origin].length === 0) { // nothing in filter list for origin
-      storeList[origin] = [cssfilter]
+      storeList[origin] = [{
+        'filter': cssfilter,
+        'isIdempotent': isIdempotent(cssfilter),
+        'applied': false
+      }]
     } else { // add entry
-      storeList[origin].push(cssfilter)
+      storeList[origin].push({
+        'filter': cssfilter,
+        'isIdempotent': isIdempotent(cssfilter),
+        'applied': false
+      })
     }
     chrome.storage.local.set({ 'cosmeticFilterList': storeList })
   })
@@ -38,12 +59,12 @@ export const applyCSSCosmeticFilters = (tabData: Tab, tabId: number) => {
     }
     let hostname = tabData.hostname
     if (storeData.cosmeticFilterList[hostname] !== undefined) {
-      storeData.cosmeticFilterList[hostname].map((filter: string) => { // if the filter hasn't been applied once before, apply it and set the corresponding filter to true
+      storeData.cosmeticFilterList[hostname].map((rule: any) => { // if the filter hasn't been applied once before, apply it and set the corresponding filter to true
         if (process.env.NODE_ENV === 'shields_development') {
-          console.log('applying filter', filter)
+          console.log('applying filter', rule.filter)
         }
         chrome.tabs.insertCSS({
-          code: `${filter} {display: none;}`,
+          code: `${rule.filter} {display: none;}`,
           runAt: 'document_start'
         })
       })
@@ -51,15 +72,64 @@ export const applyCSSCosmeticFilters = (tabData: Tab, tabId: number) => {
   })
 }
 
-chrome.runtime.onConnect.addListener(function (port) {
-  console.assert(port.name === 'knockknock')
-  port.onMessage.addListener(function (msg) {
-    if (msg.joke === 'Knock knock') {
-      port.postMessage({ question: 'Who\'s there?' })
-    } else if (msg.answer === 'Madame') {
-      port.postMessage({ question: 'Madame who?' })
-    } else if (msg.answer === 'Madame... Bovary') {
-      port.postMessage({ question: "I don't get it." })
+function isIdempotent (str: String) {
+  // if string contains a non-idempotent string, the selector is not idempotent
+  //   https://www.w3schools.com/cssref/css_selectors.asp
+
+  let nonIdempotentStrings = [
+    ':active',
+    '::after',
+    '::before',
+    ':checked',
+    ':default',
+    ':disabled',
+    ':empty',
+    ':enabled',
+    ':first-child',
+    '::first-letter',
+    '::first-line',
+    ':first-of-type',
+    ':focus',
+    ':hover',
+    ':last-child',
+    ':last-of-type',
+    ':nth-child',
+    ':nth-last-child',
+    ':nth-last-of-type',
+    ':nth-of-type']
+
+  for (let i = 0; i < nonIdempotentStrings.length; i++) {
+    if (str.includes(nonIdempotentStrings[i])) {
+      return false
     }
-  })
-})
+  }
+  return true
+}
+
+/*
+cosmeticFilterList: {
+  domain.com: {
+    {
+      filter: string,
+      isIdempotent: boolean,
+      applied: boolean
+    },
+    {
+    }
+  }
+  domain2.com: {...}
+}
+
+*/
+// chrome.runtime.onConnect.addListener(function (port) {
+//   console.assert(port.name === 'knockknock')
+//   port.onMessage.addListener(function (msg) {
+//     if (msg.joke === 'Knock knock') {
+//       port.postMessage({ question: 'Who\'s there?' })
+//     } else if (msg.answer === 'Madame') {
+//       port.postMessage({ question: 'Madame who?' })
+//     } else if (msg.answer === 'Madame... Bovary') {
+//       port.postMessage({ question: "I don't get it." })
+//     }
+//   })
+// })
